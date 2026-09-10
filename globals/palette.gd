e1f663c5d5813863eed3b83a9a9ae86ddd73be40
Dazorn <marcodz554@gmail.com>
2026-09-10 14:10:28 +0200

    First scene manager version
    Supports scenes switching and current scene reloading, fade out and fade in.

--- globals/palette.gd
+++ globals/palette.gd
@@ -2,6 +2,8 @@ extends Node
 
 ## Emits everytime the active palette is changed
 signal palette_changed(colors: Array[Color])
+## Emits during scenes' transitions each time a fading step is met
+signal fade_step_changed(step: int)
 
 # Source colors, DON'T CHANGE!
 # They're used to understand how to replace colors when switching palettes
@@ -9,6 +11,11 @@ const SRC_LIGHTEST := Color(1.0, 1.0, 1.0)
 const SRC_LIGHT := Color(2.0 / 3.0, 2.0 / 3.0, 2.0 / 3.0)
 const SRC_DARK := Color(1.0 / 3.0, 1.0 / 3.0, 1.0 / 3.0)
 const SRC_DARKEST := Color(0.0, 0.0, 0.0)
+
+# Fading standard values, change frames for a different feeling
+const FADING_STEP_LOWER_BOUND := -3
+const FADING_STEP_UPPER_BOUND := 3
+const FADING_FRAMES_PER_STEP := 8
 
 # Active palette colors
 # They're loaded at runtime using .gpl files
@@ -19,9 +26,12 @@ var darkest := Color.from_rgba8(15, 56, 15)
 
 # Currently loaded .gpl files
 var _loaded: Dictionary = {}
+var _fade_step: int = 0
+var _is_fading: bool = false
 
 # On ready it sets the clear color and load palettes from folder
 func _ready() -> void:
+	process_mode = Node.PROCESS_MODE_ALWAYS
 	RenderingServer.set_default_clear_color(SRC_DARKEST)
 	_load_gpl_files_from_folder("res://assets/palettes")
 
@@ -33,6 +43,10 @@ func get_active_palette() -> Array[Color]:
 ## Returns all the loaded palettes' names
 func get_all_palette_names() -> Array[String]:
 	return _loaded.keys()
+
+## Returns current fading step value
+func get_fade_step() -> int:
+	return _fade_step
 
 ## Switches to a loaded palette, using it's name.
 ## The name must be the filename of a palette under the palettes' folder (without ".gpl")
@@ -47,6 +61,42 @@ func switch_to_palette(palette_name: String) -> bool:
 	darkest = palette[3]
 	palette_changed.emit(get_active_palette())
 	return true
+
+
+## Fades out current scene
+## Replaces all colors with the darkest one
+func fade_out(frames_per_step: int = FADING_FRAMES_PER_STEP) -> void:
+	if _is_fading:
+		return
+	_is_fading = true
+	var start_step := _fade_step - 1
+	for step in range(start_step, FADING_STEP_LOWER_BOUND - 1, -1):
+		_set_fade_step(step)
+		await _wait_for_frames(frames_per_step)
+	_is_fading = false
+
+## Fades in current scene
+## Restores color values back to normal
+func fade_in(frames_per_step: int = FADING_FRAMES_PER_STEP) -> void:
+	if _is_fading:
+		return
+	_is_fading = true
+	var start_step := _fade_step + 1
+	for step in range(start_step, 1):
+		_set_fade_step(step)
+		await _wait_for_frames(frames_per_step)
+	_is_fading = false
+
+func _wait_for_frames(frames: int) -> void:
+	for _f in frames:
+		await get_tree().process_frame
+
+func _set_fade_step(value: int) -> void:
+	var new_step: int = clamp(value, FADING_STEP_LOWER_BOUND, FADING_STEP_UPPER_BOUND)
+	if _fade_step == new_step:
+		return
+	_fade_step = new_step
+	fade_step_changed.emit(_fade_step)
 
 func _load_gpl_files_from_folder(dir_path: String) -> void:
 	_loaded.clear()
