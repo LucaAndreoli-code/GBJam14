e2ea91685942fd067393eb093638b8e593520921
Dazorn <marcodz554@gmail.com>
2026-09-14 15:21:51 +0200

    Updated shader to use multiple light sources

--- scripts/core/screen_visibility_manager.gd
+++ scripts/core/screen_visibility_manager.gd
@@ -1,17 +1,36 @@
 extends SubViewportContainer
 
+@export_range(0.0, 10.0, 0.1) var flicker_speed: float = 4.0
+@export_range(0.0, 2.0, 0.1) var flicker_amplitude: float = 0.5
+
+const LIGHT_SOURCE_RADIUS_METHOD_SIGNATURE = "get_light_radius"
+
 func _process(_delta: float) -> void:
 	var light_sources := get_tree().get_nodes_in_group("light_sources")
-	if light_sources.size() == 0:
-		return
-	var source := light_sources[0] as Node2D
-	if source == null:
-		push_warning("Found light source which is not Node2D: %s" % light_sources[0].name)
-		return
-	_apply(source)
+	var lights: Array[Vector3] = []
+	for source in light_sources:
+		if source is not Node2D:
+			push_warning("Found light source which is not Node2D: %s" % source.name)
+			continue
+		if not source.has_method(LIGHT_SOURCE_RADIUS_METHOD_SIGNATURE):
+			push_warning("Found light source which has no %s: %s" % 
+			[LIGHT_SOURCE_RADIUS_METHOD_SIGNATURE, source.name])
+			continue
+		var source_node := source as Node2D
+		var source_pos : Vector2 = round(source_node.global_position)
+		var source_radius: float = source.call(LIGHT_SOURCE_RADIUS_METHOD_SIGNATURE)
+		var source_radius_offset := float(source.get_instance_id() % 100 / 10.0)
+		var source_radius_flickered := _calc_flickering(source_radius, source_radius_offset)
+		lights.append(Vector3(source_pos.x, source_pos.y, source_radius_flickered))
+	_apply(lights)
 
-func _apply(source: Node2D) -> void:
+func _calc_flickering(radius: float, offset: float) -> float:
+	var time := offset + Time.get_ticks_msec() / 1000.0
+	return round(radius + sin(time * flicker_speed) * flicker_amplitude)
+
+func _apply(lights: Array[Vector3]) -> void:
 	if material == null:
 		push_error("No material found on VisibilityContainer: visibility not applied")
 		return
-	material.set_shader_parameter("light_pos", round(source.global_position))
+	material.set_shader_parameter("lights", lights)
+	material.set_shader_parameter("lights_count", lights.size())
