302860a04b71fab86f3ffc446f4e1961ebf056b1
Dazorn <marcodz554@gmail.com>
2026-09-16 12:04:35 +0200

    Minigame working with dungeon

--- entities/dungeon/dig/dig_interactable.gd
+++ entities/dungeon/dig/dig_interactable.gd
@@ -1,10 +1,9 @@
 extends DungeonInteractable
 
-const MINIGAME_SCENE: Resource = preload("res://scenes/demo/scene_manager/scene_manager_1.tscn")
 const TILESET_SOURCE_ID: int = 1
 const TILESET_ATLAS_COORDS: Vector2i = Vector2i(3, 3)
 
-@export var scene_root: Node2D
+@export var scene_root: DungeonManager
 @export var tilemap: TileMapLayer
 
 var _is_busy: bool = false
@@ -21,19 +20,7 @@ func interact(_player: PlayerDungeonController) -> void:
 	if _is_busy:
 		return
 	_is_busy = true
-	SignalBus.visibility_shader_toggled.emit(false)
-	var layer := CanvasLayer.new()
-	layer.layer = 10
-	layer.process_mode = Node.PROCESS_MODE_WHEN_PAUSED
-	var minigame: Node = MINIGAME_SCENE.instantiate()
-	layer.add_child(minigame)
-	scene_root.add_child(layer)
-	# TODO: await sul minigame
-	#await minigame.finished
-	layer.queue_free()
-	_is_busy = false
-	_update_tile()
-	queue_free()
+	scene_root.start_digging_minigame()
 
 func _update_tile() -> void:
 	var cell := tilemap.local_to_map(tilemap.to_local(global_position))
--- scripts/game/dungeon_manager.gd
+++ scripts/game/dungeon_manager.gd
@@ -1,10 +1,12 @@
+class_name DungeonManager
 extends Node2D
 
-const TICK_INTERVAL := 1.0
+const MINIGAME_SCENE_PATH: String = "res://scenes/minigames/digging/digging_minigame.tscn"
 
 @export var torch_duration_seconds: int = 120
 @export var gameover_duration_seconds: int = 60
 
+@onready var _game_time: GameTime = $GameTime
 @onready var _level_tilemap: TileMapLayer = $DungeonTilemap
 @onready var _player: PlayerDungeonController = $PlayerDungeon
 @onready var _inventory: CanvasLayer = $InventoryLayer
@@ -15,9 +17,6 @@ var _torch: TorchManager
 var _minimap_hud: MinimapHUD
 var _torch_hud: TorchHUD
 
-var _game_time: float = 0.0
-var _game_seconds: int = 0
-var _last_tick_time: float = 0.0
 var _is_gameover_mode: bool = false
 var _gameover_timer: int = 0
 var _is_input_enabled: bool = true
@@ -29,12 +28,10 @@ func _ready() -> void:
 	_torch = TorchManager.new(torch_duration_seconds)
 	_torch.torch_ended.connect(_on_torch_ended)
 	_torch_hud = TorchHUD.new(_torch.get_remaining_duration())
+	SignalBus.game_second_tick.connect(_on_game_second_tick)
 	SignalBus.input_enabled.connect(_on_input_enabled)
 	SignalBus.torch_refill.connect(_on_torch_refill)
 	_init_hud()
-
-func _process(delta: float) -> void:
-	_update_game_tick(delta)
 
 func _unhandled_input(event: InputEvent) -> void:
 	if not _is_input_enabled:
@@ -53,6 +50,31 @@ func _unhandled_input(event: InputEvent) -> void:
 		GameState.set_paused(true)
 		_is_inventory_opened = true
 
+func on_scene_entered(payload: Dictionary) -> void:
+	var incoming_game_time: float = payload.get("game_time", 0.0)
+	if incoming_game_time != 0.0:
+		_game_time.set_game_time(incoming_game_time)
+	var incoming_torch_duration: int = payload.get("torch_timer", 0)
+	if incoming_torch_duration != 0:
+		_torch = TorchManager.new(incoming_torch_duration)
+	if payload.has("player_position"):
+		_player.global_position = payload.get("player_position", Vector2.ZERO)
+	SignalBus.visibility_shader_toggled.emit(true)
+
+func start_digging_minigame() -> void:
+	SignalBus.visibility_shader_toggled.emit(false)
+	_minimap_hud.set_disabled(true)
+	var payload := {
+		"game_time": _game_time.get_game_time(),
+		"torch_timer": _torch.get_remaining_duration(),
+		"scene_path": scene_file_path,
+		"player_position": _player.global_position
+	}
+	SceneManager.go_to(MINIGAME_SCENE_PATH, payload)
+
+func _on_game_second_tick(_game_seconds: int) -> void:
+	_tick_gameover_timer()
+
 func _on_input_enabled(is_enabled: bool) -> void:
 	_is_input_enabled = is_enabled
 
@@ -70,19 +92,6 @@ func _init_hud() -> void:
 	if _hud_container:
 		_hud_container.add_child(_torch_hud)
 		_hud_container.add_child(_minimap_hud)
-
-func _update_game_tick(delta: float) -> void:
-	if GameState.is_paused():
-		return
-	_game_time += delta
-	SignalBus.game_tick.emit(delta, _game_time)
-	var local_delta := _game_time - _last_tick_time
-	if local_delta > TICK_INTERVAL:
-		_game_seconds += 1
-		_last_tick_time = _game_time - (local_delta - TICK_INTERVAL)
-		print("Game update: %d seconds elapsed" % _game_seconds)
-		SignalBus.game_second_tick.emit(_game_seconds)
-		_tick_gameover_timer()
 
 func _tick_gameover_timer() -> void:
 	if not _is_gameover_mode:
