1fa3da8c1d0309b2351f9d61212135bf372d3521
Dazorn <marcodz554@gmail.com>
2026-09-18 16:52:28 +0200

    Fixed composition pick algo for treasures

--- scripts/game/dungeon_manager.gd
+++ scripts/game/dungeon_manager.gd
@@ -102,10 +102,16 @@ func _setup_treasures() -> void:
 	if digging_spots_count <= 0:
 		push_warning("No digging spots found for this level!")
 		return
-	var treasures_count := randi_range( \
-		minigame_session_min_treasures * digging_spots_count, \
-		minigame_session_max_treasures * digging_spots_count)
-	var composition := TreasureUtils.pick_composition(treasures_count, level_target_points)
+	var composition := TreasureUtils.pick_composition(
+		level_target_points, \
+		digging_spots_count, \
+		minigame_session_min_treasures, \
+		minigame_session_max_treasures)
+	if composition.size() == 0:
+		push_warning("Failed to pick a composition")
+		push_warning("Digging spots: %d" % digging_spots_count)
+		push_warning("Target points: %d" % level_target_points)
+		return
 	var minigame_treasures := GameState.get_treasures_pool()
 	minigame_treasures.clear()
 	for kind in composition.keys():
