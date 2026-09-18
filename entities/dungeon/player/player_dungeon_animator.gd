2201c67be38014bf3c3c62b215a51741added59b
Dazorn <marcodz554@gmail.com>
2026-09-18 17:57:08 +0200

    Tuned the visbility shader

--- entities/dungeon/player/player_dungeon_animator.gd
+++ entities/dungeon/player/player_dungeon_animator.gd
@@ -9,15 +9,17 @@ enum AnimState {
 var _anim_player: AnimationPlayer
 var _anim_state: AnimState = AnimState.IDLE
 
+var _has_torch: bool = true
 var _direction: Vector2i = Vector2i.DOWN
 
 func _init(anim_player: AnimationPlayer) -> void:
 	_anim_player = anim_player
 
-func update(is_walking: bool, direction: Vector2i) -> void:
+func update(is_walking: bool, has_torch: bool, direction: Vector2i) -> void:
 	_anim_state = AnimState.IDLE
 	if is_walking:
 		_anim_state = AnimState.WALK
+	_has_torch = has_torch
 	_direction = direction
 
 func animate() -> void:
@@ -33,4 +35,6 @@ func _get_anim_name() -> String:
 		Vector2i.DOWN: suffix = "down"
 		Vector2i.LEFT: suffix = "left"
 		Vector2i.RIGHT: suffix = "right"
+	if not _has_torch:
+		suffix += "_no_torch"
 	return "%s_%s" % [prefix, suffix]
--- scripts/core/screen_visibility_manager.gd
+++ scripts/core/screen_visibility_manager.gd
@@ -5,6 +5,7 @@ extends SubViewportContainer
 @export var is_enabled_on_start: bool = true
 
 const LIGHT_SOURCE_RADIUS_METHOD_SIGNATURE = "get_light_radius"
+const LIGHT_SOURCE_DARKNESS_METHOD_SIGNATURE = "force_darkness"
 
 var _is_enabled: bool
 
@@ -17,7 +18,8 @@ func _process(_delta: float) -> void:
 	if GameState.is_paused():
 		return
 	var light_sources := get_tree().get_nodes_in_group(Groups.LIGHT_SOURCES)
-	var lights: Array[Vector3] = []
+	var lights: Array[Vector4] = []
+	var ratios: Array[Vector2] = []
 	for source in light_sources:
 		if source is not Node2D:
 			push_warning("Found light source which is not Node2D: %s" % source.name)
@@ -29,11 +31,18 @@ func _process(_delta: float) -> void:
 		var source_node := source as Node2D
 		var canvas_xform := source.get_viewport().get_canvas_transform()
 		var source_pos : Vector2 = (canvas_xform * source_node.global_position).round()
-		var source_radius: float = source.call(LIGHT_SOURCE_RADIUS_METHOD_SIGNATURE)
-		var source_radius_offset := float(source.get_instance_id() % 100 / 10.0)
-		var source_radius_flickered := _calc_flickering(source_radius, source_radius_offset)
-		lights.append(Vector3(source_pos.x, source_pos.y, source_radius_flickered))
-	_apply(lights)
+		var source_radius: Vector3 = source.call(LIGHT_SOURCE_RADIUS_METHOD_SIGNATURE)
+		var is_dark: bool = source.has_method(LIGHT_SOURCE_DARKNESS_METHOD_SIGNATURE) \
+				and source.call(LIGHT_SOURCE_DARKNESS_METHOD_SIGNATURE)
+		var light_radius: float
+		if is_dark:
+			light_radius = round(source_radius.x)
+		else:
+			var source_radius_offset := float(source.get_instance_id() % 100 / 10.0)
+			light_radius = _calc_flickering(source_radius.x, source_radius_offset)
+		lights.append(Vector4(source_pos.x, source_pos.y, light_radius, 1.0 if is_dark else 0.0))
+		ratios.append(Vector2(source_radius.y, source_radius.z))
+	_apply(lights, ratios)
 
 func _on_visibility_shader_toggled(is_enabled: bool) -> void:
 	_is_enabled = is_enabled
@@ -42,7 +51,7 @@ func _calc_flickering(radius: float, offset: float) -> float:
 	var time := offset + Time.get_ticks_msec() / 1000.0
 	return round(radius + sin(time * flicker_speed) * flicker_amplitude)
 
-func _apply(lights: Array[Vector3]) -> void:
+func _apply(lights: Array[Vector4], ratios: Array[Vector2]) -> void:
 	if material == null:
 		push_error("No material found on VisibilityContainer: visibility not applied")
 		return
@@ -50,3 +59,4 @@ func _apply(lights: Array[Vector3]) -> void:
 		lights = []
 	material.set_shader_parameter("lights", lights)
 	material.set_shader_parameter("lights_count", lights.size())
+	material.set_shader_parameter("ratios", ratios)
