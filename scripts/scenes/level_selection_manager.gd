3b2f4269a8c3606cbbd14d605c1b2be4b9969edf
Dazorn <marcodz554@gmail.com>
2026-09-19 15:50:34 +0200

    Completed mockup for level selection

--- scripts/scenes/level_selection_manager.gd
+++ scripts/scenes/level_selection_manager.gd
@@ -1,19 +1,21 @@
 extends Node2D
 
-const ICONS_TEXTURE_PATH: Texture2D = preload("res://assets/sprites/ui/ui_mini_icons.png")
-const SHOVEL_ICON_TEXTURE_REGION: Rect2 = Rect2(0.0, 0.0, 8.0, 8.0)
-const KEY_ICON_TEXTURE_REGION: Rect2 = Rect2(8.0, 0.0, 8.0, 8.0)
-const TORCH_ICON_TEXTURE_REGION: Rect2 = Rect2(16.0, 0.0, 8.0, 8.0)
+const LEVEL_ICON_TEXTURE: Texture2D = preload("res://assets/sprites/ui/level_selection_level_item.png")
+const LEVEL_ICON_TEXTURE_REGION: Rect2 = Rect2(0.0, 0.0, 24.0, 24.0)
+const LEVEL_SELECTED_ICON_TEXTURE_REGION: Rect2 = Rect2(24.0, 0.0, 24.0, 24.0)
 
-@onready var _list: VBoxContainer = $Container/ListContainer/List
-
-@export var levels: Array[LevelSelectionItem] = []
+@onready var _levels_container: Control = $Container/Levels
+@onready var _selection_icon: AnimatedSprite2D = $SelectionIcon
+@onready var _level_title_label: Label = $Container/LevelInfo/Control/LevelTitle
+@onready var _level_text_label: Label = $Container/LevelInfo/Control/LevelText
+@onready var _level_shovel_label: Label = $Container/LevelInfo/Control/LevelStats/Shovel/Label
+@onready var _level_key_label: Label = $Container/LevelInfo/Control/LevelStats/Key/Label
+@onready var _level_torch_label: Label = $Container/LevelInfo/Control/LevelStats/Torch/Label
 
 var _item_selected: int = 0
 
 func _ready() -> void:
 	SceneManager.get_main_scene().toggle_bottom_bar(false)
-	_build_ui()
 	_refresh_ui()
 
 func _unhandled_input(event: InputEvent) -> void:
@@ -35,61 +37,30 @@ func _unhandled_input(event: InputEvent) -> void:
 	get_viewport().set_input_as_handled()
 	_move_list_selection(dy)
 
-func _build_ui() -> void:
-	for i in range(levels.size()):
-		var level := levels[i]
-		var container := HBoxContainer.new()
-		container.add_theme_constant_override("separation", 9)
-		var style := StyleBoxFlat.new()
-		style.border_width_bottom = 1
-		style.border_color = Palette.SRC_LIGHT
-		container.add_theme_stylebox_override("normal", style)
-		var label := Label.new()
-		label.add_theme_font_override("font", GameState.get_text_font())
-		label.add_theme_font_size_override("font_size", 16)
-		label.add_theme_color_override("font_color", Palette.SRC_LIGHTEST)
-		label.size_flags_horizontal = Control.SIZE_EXPAND_FILL
-		label.text = level.text
-		container.add_child(label)
-		container.add_child(_make_icon(ICONS_TEXTURE_PATH, SHOVEL_ICON_TEXTURE_REGION, level.digging_spots))
-		container.add_child(_make_icon(ICONS_TEXTURE_PATH, KEY_ICON_TEXTURE_REGION, level.keys))
-		container.add_child(_make_icon(ICONS_TEXTURE_PATH, TORCH_ICON_TEXTURE_REGION, level.torches))
-		_list.add_child(container)
-
-func _make_icon(texture: Texture2D, rect: Rect2, amount: int) -> HBoxContainer:
-	var container := HBoxContainer.new()
-	container.add_theme_constant_override("separation", 2)
-	var atlas := AtlasTexture.new()
-	atlas.atlas = texture
-	atlas.region = rect
-	var texture_rect := TextureRect.new()
-	texture_rect.texture = atlas
-	texture_rect.stretch_mode = TextureRect.STRETCH_SCALE
-	texture_rect.texture_filter = CanvasItem.TEXTURE_FILTER_NEAREST
-	var label := Label.new()
-	label.add_theme_font_override("font", GameState.get_text_font())
-	label.add_theme_font_size_override("font_size", 16)
-	label.add_theme_color_override("font_color", Palette.SRC_LIGHTEST)
-	label.text = "%02d" % amount
-	container.add_child(texture_rect)
-	container.add_child(label)
-	return container
-
 func _refresh_ui() -> void:
-	for index in _list.get_child_count():
-		var container := _list.get_child(index) as HBoxContainer
-		var level := levels[index]
-		if level == null or container == null:
-			continue
-		var color := Palette.SRC_LIGHTEST
-		if level.disabled:
-			color = Palette.SRC_DARK
-		elif index == _item_selected:
-			color = Palette.SRC_LIGHT
-		container.get_child(0).add_theme_color_override("font_color", color)
+	var levels_count := _levels_container.get_child_count()
+	for index in levels_count:
+		var node := _levels_container.get_child(index)
+		if node == null or node is not LevelSelectionItem:
+			return
+		var tex_rect := node as TextureRect
+		var atlas := AtlasTexture.new()
+		atlas.atlas = LEVEL_ICON_TEXTURE
+		atlas.region = LEVEL_SELECTED_ICON_TEXTURE_REGION if index == _item_selected else LEVEL_ICON_TEXTURE_REGION
+		tex_rect.texture = atlas
+		if _item_selected == index:
+			if _selection_icon:
+				_selection_icon.position = tex_rect.global_position
+				_selection_icon.position.x += tex_rect.size.x / 2
+			var level := node as LevelSelectionItem
+			_level_title_label.text = level.title
+			_level_text_label.text = level.short_text
+			_level_shovel_label.text = "%02d" % level.digging_spots
+			_level_key_label.text = "%02d" % level.keys
+			_level_torch_label.text = "%02d" % level.torches
 
 func _move_list_selection(dy: int) -> void:
-	var count := _list.get_child_count()
+	var count := _levels_container.get_child_count()
 	if count == 0:
 		return
 	var index := clampi(_item_selected + dy, 0, count - 1)
@@ -99,7 +70,8 @@ func _move_list_selection(dy: int) -> void:
 	_refresh_ui()
 
 func _confirm_level() -> void:
-	if _item_selected >= 0 and _item_selected < levels.size():
-		var level := levels[_item_selected]
+	var levels_count := _levels_container.get_child_count()
+	if _item_selected >= 0 and _item_selected < levels_count:
+		var level := _levels_container.get_child(_item_selected)
 		if level and level.level_scene != null:
 			SceneManager.go_to(level.level_scene.resource_path)
