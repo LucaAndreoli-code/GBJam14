6f4cb330d7eb321908de8f807d2f7ccecf3fc6b4
Dazorn <marcodz554@gmail.com>
2026-09-20 14:20:48 +0200

    Yep, the real final polish

--- scripts/scenes/main.gd
+++ scripts/scenes/main.gd
@@ -8,6 +8,7 @@ extends Control
 
 @onready var _game_viewport: SubViewport = $PaletteContainer/Screen/VisibilityContainer/GameWorld
 @onready var _bottom_bar: Control = $PaletteContainer/Screen/BottomBar
+@onready var _music_player: AudioStreamPlayer2D = $Music/Music
 
 func _ready() -> void:
 	GameState.set_fonts(title_font, text_font, little_font)
@@ -16,7 +17,14 @@ func _ready() -> void:
 		push_warning("No initial scene set on Main!")
 	else:
 		SceneManager.go_to(initial_scene.resource_path, {})
+	SignalBus.music_level_changed.connect(_on_music_level_changed)
+
+func get_music_player() -> AudioStreamPlayer2D:
+	return _music_player
 
 func toggle_bottom_bar(is_enabled: bool) -> void:
 	if _bottom_bar and _bottom_bar.visible != is_enabled:
 		_bottom_bar.visible = is_enabled
+
+func _on_music_level_changed(level: float) -> void:
+	_music_player.volume_db = linear_to_db(level)
--- scripts/scenes/title_screen_manager.gd
+++ scripts/scenes/title_screen_manager.gd
@@ -10,6 +10,7 @@ const SETTINGS_SCENE_PATH: String = "res://scenes/game/game_settings.tscn"
 @onready var _menu: VBoxContainer = $Container/Menu
 @onready var _hints: Control = $Hints
 
+var _music_player: AudioStreamPlayer2D
 var _item_selected: int = 0
 var _is_input_enabled: bool = false
 
@@ -17,6 +18,7 @@ func _ready() -> void:
 	SignalBus.visibility_shader_toggled.emit(false)
 	SceneManager.get_main_scene().toggle_bottom_bar(false)
 	await get_tree().create_timer(1.5).timeout
+	_music_player = SceneManager.get_main_scene().get_music_player()
 	_splash_screen_anim.play("default")
 	_splash_screen_anim.animation_finished.connect(_on_splash_screen_finished)
 
@@ -42,6 +44,8 @@ func _unhandled_input(event: InputEvent) -> void:
 
 func _on_splash_screen_finished():
 	_splash_screen.visible = false
+	# Force the track to start at 0 seconds to play the intro
+	_music_player.play(0.0)
 	var logo_move_tween := create_tween()
 	logo_move_tween.tween_property(_logo, "global_position", Vector2(10.0, 1.0), 1.0)
 	await logo_move_tween.finished
